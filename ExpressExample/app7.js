41bf4fd1bdced0cc9cd81d2dc9a457d2bb799084
unknown <suyoung154@gmail.com>
2018-07-24 16:26:08 +0900

    18-07-24_1

---
+++ ExpressExample/app7.js
@@ -0,0 +1,45 @@
+// Express 기본 모듈 불러오기
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
+
+// Express 미들웨어 불러오기
+var bodyParser = require('body-parser'),
+    static = require('serve-static');
+
+// 익스프레스 객체 생성
+var app = express();
+
+// 기본 속성 설정
+app.set('port', process.env.PORT || 3000);
+
+// body-parser를 사용해 application/x-www-form-urlencoded 파싱
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// body-parser를 사용해 applicaiton/json 파싱
+app.use(bodyParser.json());
+
+app.use(static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    console.log('첫 번째 미들웨어에서 요청을 처리함');
+
+    next();
+});
+
+app.use(function(req, res, next) {
+    console.log('두 번째 미들웨어에서 요청을 처리함.');
+
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+    res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+    res.write('<div><p>Param id : ' + paramId + '</p></div>');
+    res.write('<div><p>ParamPassword : ' + paramPassword + '</p></div>');
+    res.end();
+});
+
+http.createServer(app).listen(3000, function() {
+    console.log('Express 서버가 3000번 포트에서 시작됨.');
+});
